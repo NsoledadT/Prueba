8b67b4e04af1c81cac4fbc900b994bf0ef508872
soledadt2013 <soledadt2013@gmail.com>
2014-03-25 19:04:35 -0300

    tp - inicio animacion

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+   //Aquí van todas las acciones del documento.
+  
+     $('#plato_maki').click(function() {
+     $('.recuadro_platos_1').slideToggle("slow");
+      });
+	  
+    $('#plato_geishas').click(function() {
+     $('.recuadro_platos_2').slideToggle("slow");
+      });
+	  
+	   $('#plato_rolls').click(function() {
+     $('.recuadro_platos_3').slideToggle("slow");
+      });
+	  
+	 
+ });
+  
